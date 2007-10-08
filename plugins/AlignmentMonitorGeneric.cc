e97bb5b1e585971051cfc673866d375513d4f24b
cmsbuild <>
2007-10-08 15:50:16 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- CommonAlignmentMonitor-V00-07-00'.
    Sprout from master 2007-08-22 18:03:49 UTC Jim Pivarski <sha1-4b35a9abbbfdbbd8cd887eda503efe9ec3a292e0@cern.ch> 'Fixed bug in normalized profile()'
    Cherrypick from master 2007-10-08 15:50:15 UTC cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch> 'Use AlignableObjectId instead of TrackerAlignmentId.':
        plugins/AlignmentMonitorGeneric.cc

--- plugins/AlignmentMonitorGeneric.cc
+++ plugins/AlignmentMonitorGeneric.cc
@@ -1,7 +1,9 @@
 #include "TrackingTools/TrackFitters/interface/TrajectoryStateCombiner.h"
-#include "Alignment/TrackerAlignment/interface/TrackerAlignableId.h"
+#include "Alignment/CommonAlignment/interface/AlignableObjectId.h"
 
 #include "Alignment/CommonAlignmentMonitor/plugins/AlignmentMonitorGeneric.h"
+
+#include <TString.h>
 
 AlignmentMonitorGeneric::AlignmentMonitorGeneric(const edm::ParameterSet& cfg):
   AlignmentMonitorBase(cfg)
@@ -10,7 +12,7 @@ AlignmentMonitorGeneric::AlignmentMonitorGeneric(const edm::ParameterSet& cfg):
 
 void AlignmentMonitorGeneric::book()
 {
-  static TrackerAlignableId idMap;
+  static AlignableObjectId idMap;
 
   std::vector<std::string> residNames; // names of residual histograms
 
@@ -32,34 +34,35 @@ void AlignmentMonitorGeneric::book()
 
     hists.resize(nResidName, 0);
 
-    TrackerAlignableId::UniqueId id = idMap.alignableUniqueId(ali);
+    align::ID id = ali->id();
+    align::StructureType type = ali->alignableObjectId();
 
     for (unsigned int n = 0; n < nResidName; ++n)
     {
       const std::string& name = residNames[n];
 
-      std::ostringstream histName;
-      std::ostringstream histTitle;
+      TString histName(name.c_str());
+      histName += Form("_%s_%d", idMap.typeToName(type).c_str(), id);
+      histName.ReplaceAll(" ", "");
 
-      histName << name << id.first;
-      histTitle << name << " for " << idMap.alignableTypeIdToName(id.second)
-		<< " with ID " << id.first;
+      TString histTitle(name.c_str());
+      histTitle += Form(" for %s with ID %d (subdet %d)",
+			idMap.typeToName(type).c_str(),
+			id, DetId(id).subdetId());
 
-      TH1F* hist = new TH1F(histName.str().c_str(), histTitle.str().c_str(),
-			    nBin_, -5., 5.);
-
+      TH1F* hist = new TH1F(histName, histTitle, nBin_, -5., 5.);
       hists[n] = static_cast<TH1F*>( add("/iterN/" + name + '/', hist) );
     }
   }
 
   m_trkHists.resize(6, 0);
 
-  m_trkHists[0] = new TH1F("pt"  , "track p_{t} (GeV)" , nBin_,   0.0, 10.0);
+  m_trkHists[0] = new TH1F("pt"  , "track p_{t} (GeV)" , nBin_,   0.0,100.0);
   m_trkHists[1] = new TH1F("eta" , "track #eta"        , nBin_, - 3.0,  3.0);
   m_trkHists[2] = new TH1F("phi" , "track #phi"        , nBin_, -M_PI, M_PI);
-  m_trkHists[3] = new TH1F("d0"  , "track d0 (cm)"     , nBin_, - 0.1,  0.1);
-  m_trkHists[4] = new TH1F("dz"  , "track dz (cm)"     , nBin_, -10.0, 10.0);
-  m_trkHists[5] = new TH1F("chi2", "track #chi^{2}/dof", nBin_,   0.0, 10.0);
+  m_trkHists[3] = new TH1F("d0"  , "track d0 (cm)"     , nBin_, -0.02, 0.02);
+  m_trkHists[4] = new TH1F("dz"  , "track dz (cm)"     , nBin_, -20.0, 20.0);
+  m_trkHists[5] = new TH1F("chi2", "track #chi^{2}/dof", nBin_,   0.0, 20.0);
 
   for (unsigned int h = 0; h < m_trkHists.size(); ++h)
     m_trkHists[h] = static_cast<TH1F*>( add("/iterN/", m_trkHists[h]) );
@@ -88,24 +91,23 @@ void AlignmentMonitorGeneric::event(const edm::EventSetup&,
       {
 	const Alignable* ali = pNavigator()->alignableFromDetId( hit.geographicalId() );
 
-	std::map<const Alignable*, Hist1Ds>::iterator h = m_resHists.find(ali);
-
-	while ( h == m_resHists.end() && ( ali = ali->mother() ) )
-	  h = m_resHists.find(ali);
-
-	if ( h != m_resHists.end() )
-	{
-	  TrajectoryStateOnSurface tsos = tsoscomb( meas.forwardPredictedState(), meas.backwardPredictedState() );
-
-	  align::LocalVector res = tsos.localPosition() - hit.localPosition();
-	  LocalError err1 = tsos.localError().positionError();
-	  LocalError err2 = hit.localPositionError();
-
-	  float errX = std::sqrt( err1.xx() + err2.xx() );
-	  float errY = std::sqrt( err1.yy() + err2.yy() );
-
-	  h->second[charge > 0 ? 0 : 1]->Fill(res.x() / errX);
-	  h->second[charge > 0 ? 2 : 3]->Fill(res.y() / errY);
+	while (ali) {
+	  std::map<const Alignable*, Hist1Ds>::iterator h = m_resHists.find(ali);
+	  if ( h != m_resHists.end() )
+	    {
+	      TrajectoryStateOnSurface tsos = tsoscomb( meas.forwardPredictedState(), meas.backwardPredictedState() );
+	      
+	      align::LocalVector res = tsos.localPosition() - hit.localPosition();
+	      LocalError err1 = tsos.localError().positionError();
+	      LocalError err2 = hit.localPositionError();
+	      
+	      float errX = std::sqrt( err1.xx() + err2.xx() );
+	      float errY = std::sqrt( err1.yy() + err2.yy() );
+	      
+	      h->second[charge > 0 ? 0 : 1]->Fill(res.x() / errX);
+	      h->second[charge > 0 ? 2 : 3]->Fill(res.y() / errY);
+	    }
+	  ali = ali->mother();
 	}
       }
     }
@@ -120,5 +122,5 @@ void AlignmentMonitorGeneric::event(const edm::EventSetup&,
 }
 
 #include "Alignment/CommonAlignmentMonitor/interface/AlignmentMonitorPluginFactory.h"
-
 DEFINE_EDM_PLUGIN(AlignmentMonitorPluginFactory, AlignmentMonitorGeneric, "AlignmentMonitorGeneric");
+  
